0067416a63645edaa8c8e7d18095f32c99977ea2
greatkay-olowo <oluwakayode.alao@gmail.com>
2020-05-05 21:36:46 +0100

    push code to github

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -6,7 +6,7 @@ const Checkout = () => {
     <div>
       <Layout>
         <div className="row">
-          <div className="col-md-4 order-md-2 mb-4">
+          <div className="col-md-4 order-md-2 mb-4 shadow">
             <h4 className="d-flex justify-content-between align-items-center mb-3">
               <span className="text-muted">Your cart</span>
               <span className="badge badge-secondary badge-pill">3</span>
