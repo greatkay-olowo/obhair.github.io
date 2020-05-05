05b5613bc7b4a3ea3ad070f27008575d88cda995
greatkay-olowo <oluwakayode.alao@gmail.com>
2020-05-05 15:39:30 +0100

    Updates

---
+++ precache-manifest.2a68d1bcf46ed625b0e3bb5a2b062d69.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c53179bcaaa4a2cf3bda0465434a03",
+    "url": "/obhair.github.io/index.html"
+  },
+  {
+    "revision": "c7fc1a1156409c88ffd6",
+    "url": "/obhair.github.io/static/css/2.a062f155.chunk.css"
+  },
+  {
+    "revision": "8c531c8dfa16ae7fb0ca",
+    "url": "/obhair.github.io/static/css/main.cddf5a1c.chunk.css"
+  },
+  {
+    "revision": "c7fc1a1156409c88ffd6",
+    "url": "/obhair.github.io/static/js/2.f5307199.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/obhair.github.io/static/js/2.f5307199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c531c8dfa16ae7fb0ca",
+    "url": "/obhair.github.io/static/js/main.8093fe99.chunk.js"
+  },
+  {
+    "revision": "6627417465088110e681",
+    "url": "/obhair.github.io/static/js/runtime-main.2c901c33.js"
+  },
+  {
+    "revision": "ca597ac4284d2623873bffea7023ea4d",
+    "url": "/obhair.github.io/static/media/1.ca597ac4.jpg"
+  },
+  {
+    "revision": "5ea91ec7f4c97541d2f36d84b12d71f2",
+    "url": "/obhair.github.io/static/media/2.5ea91ec7.jpg"
+  },
+  {
+    "revision": "69b98ef5b7eda8f99b7bbb235916238d",
+    "url": "/obhair.github.io/static/media/3.69b98ef5.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/obhair.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/obhair.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/obhair.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/obhair.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d16fe73346204aa1e6f07594bcf3769.js"
+  "/obhair.github.io/precache-manifest.2a68d1bcf46ed625b0e3bb5a2b062d69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/obhair.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
