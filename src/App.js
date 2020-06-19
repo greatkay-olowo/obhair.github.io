23dcd83af3538ef8fc7e848a34167bf5e581b1e2
greatkay-olowo <oluwakayode.alao@gmail.com>
2020-06-19 01:03:37 +0100

    add product add, edit and delete from the back end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 // const MainApp = lazy(() => import("./pages/mainapp"));
 import MainApp from "./pages/mainapp";
 import Admin from "./pages/admin";
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
 
 import Layout from "../pages/layout";
 import "./404.css";
@@ -19,12 +18,11 @@ const PageNotFound = () => {
           <br />* Send us an e-mail about this error and try later.
         </p>
         <nav className="nav1">
-
           &nbsp;|&nbsp;
           <Link to="/" className="link1">
             home
           </Link>
-           &nbsp;|&nbsp;
+          &nbsp;|&nbsp;
         </nav>
       </main>
     </Layout>
